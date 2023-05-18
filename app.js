ce86b67032f44d5bcd836c2355f9282c0b5fb82c
ishanm17 <mulchandaniishan@gmail.com>
2023-05-18 11:49:29 +0530

    fix: app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/', (req, res)=>{
 mongoose.set('strictQuery', false);
 
 mongoose
-    .connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .connect(process.env.MONGO_URI)
     .then((res) => console.log('Connected to MongoDB...'))
     .catch((err) => console.log(err));
 
